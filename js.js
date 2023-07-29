6bbdee25c824add075886d651d19226ffb2d019e
Maxwell <nanayawfixing@gmail.com>
2023-07-29 10:30:19 +0000

    good day

--- js.js
+++ js.js
@@ -1,112 +1,76 @@
-console.log("hello world");
+// Wait for the DOM to be ready
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the coin selection area and the boards
+  const coinSelection = document.getElementById("coinSelection");
+  const boards = document.querySelectorAll(".board"); // Select all elements with class "board"
 
-// console.log(timePassed);
+  // Attach click event listeners to each coin option
+  coinSelection.querySelectorAll("img").forEach(coin => {
+    coin.addEventListener("click", function () {
+      // Deselect the previously selected coin (if any)
+      if (selectedCoin) {
+        selectedCoin.classList.remove("selected");
+      }
 
-
-    const decrementButton = document.getElementById("decrement");
-  const incrementButton = document.getElementById("increment");
-  const result = document.getElementById("btnMultiBetCount");
-  let currentValue = 1;
-
-  decrementButton.addEventListener("click", function() {
-    if (currentValue > 1) {
-      currentValue -= 1;
-      result.value = 'X' + currentValue;
-    }
+      // Select the current coin
+      selectedCoin = this;
+      selectedCoin.classList.add("selected");
+    });
   });
 
-  incrementButton.addEventListener("click", function() {
-    currentValue += 1;
-    result.value = 'X' + currentValue;
+  // Variables to keep track of the selected coin and board
+  let selectedCoin = null;
+  let selectedBoard = null;
+
+  // Track the number of stacked coins for each board
+  const stackedCoins = {
+    board1: 0,
+    board2: 0,
+    board3: 0,
+    board4: 0,
+    board5: 0
+  };
+
+  // Attach click event listeners to each board
+  boards.forEach(board => {
+    board.addEventListener("click", function () {
+      if (selectedCoin && selectedBoard !== this) {
+        // Get the board ID
+        selectedBoard = this.id;
+
+        // Calculate the offset from coin to selected board
+        const coinRect = selectedCoin.getBoundingClientRect();
+        const boardRect = this.getBoundingClientRect();
+        const offsetX = boardRect.left - coinRect.left;
+        const offsetY = boardRect.top - coinRect.top - (stackedCoins[selectedBoard] * 50);
+
+        // Create a new coin image and append it to the body
+        const newCoin = selectedCoin.cloneNode();
+        newCoin.style.position = "absolute";
+        newCoin.style.width = coinRect.width + "px"; // Set width explicitly
+        newCoin.style.height = coinRect.height + "px"; // Set height explicitly
+        newCoin.style.left = coinRect.left + "px";
+        newCoin.style.top = coinRect.top + "px";
+        document.body.appendChild(newCoin);
+
+        // Increment the number of stacked coins for the selected board
+        stackedCoins[selectedBoard]++;
+
+        // Animate the new coin to the selected board's position
+        anime({
+          targets: newCoin,
+          translateX: offsetX,
+          translateY: offsetY,
+          duration: 1000, // Animation duration in milliseconds
+          easing: "easeOutQuart", // Easing function for smooth animation
+          complete: function () {
+            // Deselect the coin and board after animation
+            selectedCoin.classList.remove("selected");
+            selectedCoin = null;
+            selectedBoard = null;
+          }
+        });
+      }
+    });
   });
-
-
-  const button = document.querySelector(".btn-mask-balance");
-  const mask_balance = document.querySelector(".mask-balance");
-  const unmask_balance = document.querySelector(".unmasked-balance");
-  let isMasked = false;
-
-  button.addEventListener("click", function() {
-    if (isMasked) {
-        mask_balance.classList.toggle('visually-hidden')
-        unmask_balance.classList.toggle('visually-hidden');
-        button.className = 'bi bi-eye'
-        isMasked = false;
-    } else {
-        button.className = 'bi bi-eye-slash'
-        mask_balance.classList.toggle('visually-hidden')
-        unmask_balance.classList.toggle('visually-hidden');
-      isMasked = true;
-    }
-  });
-
-
-  const master = document.getElementById("master");
-  const slaves = document.querySelectorAll(".slave");
-
-  console.log(master)
-  master.addEventListener("change", function () {
-    for (let slave of slaves) {
-      slave.checked = master.checked;
-    }
-  });
-
-// $(".guestCounter li").on("click", function (element) {
-//     let operationType = $(this).attr("data-btn-type");
-//     // let oldValue = $(this).parent().find("input").val();
-//     console.log(operationType);
-//     let result = document.getElementById("result")
-//     let results = document.getElementById("result").value.replace('X', '');
-    
-//     let currentValue=1;
-//     // let newVal;
-//     if (operationType == "increment") {
-//         result = Number(results ++);
-//         console.log(results);
-//         result.value = 'X' + results;
-//     } 
-//     else {
-
-//         // Don't allow decrementing below zero
-
-//         if (currentValue > 1) {
-//             currentValue --;
-//             result.value = 'X' + currentValue;
-//             console.log(currentValue);
-
-//           }
-//     }
-//     console.log(results);
-//     console.log(currentValue);
-
-//     $(this).parent().find("input").val(result.value);
-
-
-
-    
-
-// //   decrementButton.addEventListener("click", function() {
-    
-// //   });
-
-// //   incrementButton.addEventListener("click", function() {
-    
-// //   });
-
-// // let newVal;
-// //     if (operationType == "increment") {
-// //          newVal = `${parseFloat(oldValue) + 1}x`;
-// //     } else {
-
-// //         // Don't allow decrementing below zero
-
-// //         let oldValues = oldValue.replace('x', '')
-
-// //         if (oldValues > 1) {
-// //              newVal = `${ parseFloat(oldValue) - 1}x`;
-// //         } else {
-// //             newVal = "1x";
-// //         }
-// //     }
-// //     $(this).parent().find("input").val(newVal);
-// });
+});
